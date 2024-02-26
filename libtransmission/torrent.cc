d259847c3bf6b598c9c3e42562dbe35c4f761c48
Cœur <coeur@gmx.fr>
2024-02-26 09:31:35 +0800

    Make torrent verify on completion configurable via settings

--- libtransmission/torrent.cc
+++ libtransmission/torrent.cc
@@ -1864,7 +1864,7 @@ void tr_torrent::recheck_completeness()
 
         completeness_ = new_completeness;
 
-        if (is_done() && was_running)
+        if (is_done() && was_running && session->shouldFullyVerifyCompleteTorrents())
         {
             tr_torrentVerify(this);
         }
