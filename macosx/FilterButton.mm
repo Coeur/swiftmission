8ef5d2d89bfbd7d57832a08378053ca7ad110cc0
Cœur <coeur@gmx.fr>
2026-08-31 02:22:22 +0200

    fix the disappearing filterbar buttons

--- macosx/FilterButton.mm
+++ macosx/FilterButton.mm
@@ -5,6 +5,13 @@
 #import "FilterButton.h"
 #import "NSStringAdditions.h"
 
+@interface FilterButton ()
+
+/// Button on its left.
+@property(nonatomic) IBOutlet FilterButton* previousButton;
+
+@end
+
 @implementation FilterButton
 
 - (instancetype)initWithCoder:(NSCoder*)coder
@@ -14,6 +21,26 @@
         _count = NSNotFound;
     }
     return self;
+}
+
+- (void)setFrame:(NSRect)frame
+{
+    // The native NSButtonTextField is removed from the view hierarchy at unpredictable times by `[NSButtonAppearanceBasedVisualProvider removeTextField]`.
+    // That removal is later followed by the re-addition of a new textfield.
+    // But the containing NSStackView is also misframing the buttons, ignoring layout constraints.
+    // Apple switched development to SwiftUI and will probably not fix this, so we override the frame ourselves.
+
+    if (frame.size.width < 20)
+    {
+        // Prevent buttons from disappearing
+        frame.size.width = 20;
+    }
+    if (self.previousButton)
+    {
+        // Prevents the NSStackView superposing the buttons
+        frame.origin.x = NSMaxX(self.previousButton.frame) + 1;
+    }
+    [super setFrame:frame];
 }
 
 - (void)setCount:(NSUInteger)count
